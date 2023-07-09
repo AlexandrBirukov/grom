3084384e6117b49181c9e730a9634a4152d46197
Alexandr <birukov.system@gmail.com>
2023-07-09 14:32:07 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(function () {
+
+    // ----- Мобильное меню -----
+
+    const header = $('#header');
+    let scrollPrev = 0;
+
+    $(window).scroll(function () {
+        let scrolled = $(window).scrollTop();
+
+        if (scrolled > 100 && scrolled > scrollPrev) {
+            if (!header.hasClass('header-open')) {
+                header.addClass('out');
+            }
+        } else {
+            header.removeClass('out');
+        }
+        scrollPrev = scrolled;
+    });
+
+    // ----- Навигация -----
+
+    $('#nav-icon').click(function () {
+        $(this).toggleClass('open'); // анимация кнопки
+        $('#header-nav').toggleClass('mobile-nav'); // анимация навигации
+        $('.phone').toggleClass('mobile-phone'); // анимация телефона
+    });
+
+
+    // ----- Прокрутка до якоря -----
+
+    $('.nav-link').on('click', function (event) {
+        event.preventDefault();
+
+        $('#nav-icon').removeClass('open');
+        $('#header-nav').removeClass('mobile-nav'); // анимация навигации
+        $('.phone').removeClass('mobile-phone'); // анимация телефона
+
+        let destination = $($(this).attr('href')).offset().top - 70;
+        $('html:not(:animated),body:not(:animated)').animate({ scrollTop: destination }, 600);
+    });
+
+    // --- Карта ---
+
+    ymaps.ready(init);
+    var myMap;
+
+    function init() {
+        myMap = new ymaps.Map('map', {
+            center: [59.945557564159294, 30.348359999999975],
+            zoom: 17,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+        event_item = new ymaps.Placemark([59.945557564159294, 30.348359999999975], {
+            balloonContent: '<strong>ГРОМ</strong><br>г. Санкт-Петербург, Литейный пр-т д.9<br>+7-921-905-24-84',
+            hintContent: '<strong>ГРОМ</strong><br>г. Санкт-Петербург, Литейный пр-т д.9<br>+7-921-905-24-84',
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#a7142c'
+        });
+        myMap.geoObjects.add(event_item);
+        myMap.behaviors.disable('scrollZoom');
+    }
+});
