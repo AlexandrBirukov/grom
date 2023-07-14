8d46af4c244167a2329e27801a2eb35dfbc6e04a
Alexandr Birukov <birukov.system@gmail.com>
2023-07-14 23:39:22 +0300

    preloader

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 $(function () {
 
+    // ----- Прелоадер -----
+
+    $(window).on('load', function () {
+        function hidePreloader() {
+            $('#preloader').fadeOut('slow');
+        }
+        setTimeout(hidePreloader, 2000)
+    });
+
+    // ----- Кнопка "На верх"-----
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+
+    $('#scroll-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
     // ----- Мобильное меню -----
 
     const header = $('#header');
